8ab93404e5f2e3ca44ee35a0d3acec600e8d72e0
arieljovananda <arieljovananda@gmail.com>
2022-11-04 20:55:30 +0700

    fungsi mean for now

---
+++ src/mean.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+#dataset
+m1 = np.array([[2,0,1], [1,2,0], [0,2,4]])
+m2 = np.array([[1,1,1], [0,1,0], [1,2,2]])
+row1 = len(m1)
+col1 = len(m1[0])
+m = [[0 for i in range(col1)]for j in range(row1)]
+arr = [m1, m2]
+i = 0
+
+
+def matrixAdd (m1):
+    row1 = len(m1)
+    col1 = len(m1[0])
+    m = [[0 for i in range(col1)]for j in range(row1)]
+    
+    m += m1
+    return m
+    
+
+def mean (array):
+    row = len(array[0]) #baca baris dari matrix pertama
+    col = len(array[0][0]) #baca kolom dari matrix pertama
+    m = [[0 for i in range(col)]for j in range(row)] #membuat matrix berelemen nol dengan ukuran row1 x col1 
+    count = 0
+
+    if m1.shape != m2.shape:
+        return False
+    else:
+        while count < len(array):
+            m += matrixAdd(array[count]) #menambahkan semua matrix
+            count += 1
+        
+        for i in range (row):
+            for j in range (col):
+                m[i][j] = np.floor(1/count * m[i][j]) #mengalikan hasil akhir matrix dengan 1/M
+        
+        return m
+        
+        
+
+mean(arr)
+
+#output
+#[[1 0 1]
+#[0 1 0]
+#[0 2 3]]
+
+#newmat = np.array([[1,2,3], [4,5,6]])
+
+#row = len(newmat)
+#col = len(newmat[0])
+
+#print(newmat)
+#print(row)
+#print(col)
