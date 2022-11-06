764b26f087ae7a5730ee1325a075a4229a79119d
arieljovananda <arieljovananda@gmail.com>
2022-11-06 21:18:35 +0700

    nambah fungsi selisih dan eigenface

--- src/mean.py
+++ src/mean.py
@@ -3,6 +3,7 @@ import numpy as np
 #dataset
 m1 = np.array([[2,0,1], [1,2,0], [0,2,4]])
 m2 = np.array([[1,1,1], [0,1,0], [1,2,2]])
+mock_matrix = np.array([[1,-1,0], [-1,-1,0], [0,0,0]])
 row1 = len(m1)
 col1 = len(m1[0])
 m = [[0 for i in range(col1)]for j in range(row1)]
@@ -37,15 +38,28 @@ def mean (array):
                 m[i][j] = np.floor(1/count * m[i][j]) #mengalikan hasil akhir matrix dengan 1/M
         
         return m
-        
-        
 
-mean(arr)
+def selisih(array):
+    array1 = [0 for i in range(len(array))]
+    m = mean(array)
+    
+    for i in range(len(array)):
+        array1[i] = abs(array[i] - m)
+    
+    return array1
+    
 
-#output
-#[[1 0 1]
-#[0 1 0]
-#[0 2 3]]
+def eigenface(array, eigenvec):
+    array2 = [0 for i in range(len(array))]
+    arr = selisih(array)
+
+    for i in range(len(array)):
+        array2[i] = np.matmul(eigenvec,arr[i])
+    
+    return array2
+
+print(eigenface(arr, mock_matrix))
+
 
 #newmat = np.array([[1,2,3], [4,5,6]])
 
