fe6a0dfad3130479fdc13356bfceede8d14f839c
Rava Maulana <ravamaulana14@gmail.com>
2022-11-21 20:03:25 +0700

    Added gui

--- src/new_testing.py
+++ src/new_testing.py
@@ -3,85 +3,74 @@ import numpy as np
 from glob import glob
 import Eigenvalue as eigval
 import mean_selisih as mean
+from os.path import exists
 
-# Import dataset
-dataset_folder = "C:/Users/Rava Maulana/Desktop/Test Data"
-dataset_folder = glob(dataset_folder + "/*.png") + glob(dataset_folder + "/*.jpg")
+async def train_dataset(dataset_path):
+  # Import dataset
+  dataset_folder = dataset_path
 
-# Change dataset image into N^2 x 1 vector
-dataset = []
-for i in range(len(dataset_folder)):
-  dataset_image = cv2.imread(dataset_folder[i])
-  dataset_image = cv2.cvtColor(dataset_image, cv2.COLOR_BGR2GRAY)
-  dataset_image = cv2.resize(dataset_image, (256, 256))
-  dataset_image = dataset_image.reshape(256**2, 1)
-  dataset.append(dataset_image)
+  # Checks for a cache file
+  if exists(dataset_folder + "/eigen_cache.txt"):
+    weight_arr = []
+    with open(dataset_folder + "/eigen_cache.txt", 'r') as f:
+      for line in f:
+        weight_arr.append(line.split())
+    weights = np.array(weight_arr, dtype="float64")
+    return weights
 
-# Computes mean face of the dataset
-mean_face = mean.mean_selisih(dataset)[1]
-mean_face = mean_face.astype("float64")
+  dataset_folder = glob(dataset_folder + "/*.png") + glob(dataset_folder + "/*.jpg")
 
-# Computes difference between each image and mean face
-# temp_diff = dataset[i] - mean_face
-# difference = temp_diff 
-# for i in range(1, len(dataset)):
-#   temp_diff = dataset[i] - mean_face
-#   difference = np.concatenate((difference, temp_diff), axis = 1)
-difference = mean.mean_selisih(dataset)[0]
-difference = difference.astype("float64")
+  # Change dataset image into N^2 x 1 vector
+  dataset = []
+  for i in range(len(dataset_folder)):
+    dataset_image = cv2.imread(dataset_folder[i])
+    dataset_image = cv2.cvtColor(dataset_image, cv2.COLOR_BGR2GRAY)
+    dataset_image = cv2.resize(dataset_image, (256, 256))
+    dataset_image = dataset_image.reshape(256**2, 1)
+    dataset.append(dataset_image)
 
-# Computes the simplified covariance matrix
-simp_covariance = np.transpose(difference) @ difference
-simp_covariance /= len(dataset)
-simp_covariance /= len(dataset)
-simp_covariance /= len(dataset)
-simp_covariance /= len(dataset)
+  # Computes mean face of the dataset
+  mean_face = mean.mean_selisih(dataset)[1]
+  mean_face = mean_face.astype("float64")
 
-# Computes the eigenvalue of simplified covariance matrix
-# simp_eigens = np.linalg.eig(simp_covariance)[1]
-simp_eigens = eigval.eigen(simp_covariance)
-print(simp_eigens.shape)
+  # Computes difference between each image and mean face
+  difference = mean.mean_selisih(dataset)[0]
+  difference = difference.astype("float64")
 
-# Computes the eigenvalues from simplified eigenvalues
-curr_simp_eigen = simp_eigens[0:, 0]
-eigens = difference @ curr_simp_eigen.reshape(len(curr_simp_eigen), 1)
-for i in range(1, simp_eigens.shape[1]):
-  curr_simp_eigen = simp_eigens[0:, i]
-  eigens = np.concatenate((eigens, difference @ curr_simp_eigen.reshape(len(curr_simp_eigen), 1)), axis=1)
+  # Computes the simplified covariance matrix
+  simp_covariance = np.transpose(difference) @ difference
+  simp_covariance /= len(dataset)
+  simp_covariance /= len(dataset)
+  simp_covariance /= len(dataset)
+  simp_covariance /= len(dataset)
 
-# Normalize the eigenvalues to compute the eigenfaces
-curr_eigen = eigens[0:, 0].reshape(len(eigens[0:, 0]), 1)
-eigenfaces = curr_eigen/np.linalg.norm(curr_eigen)
-for i in range(1, eigens.shape[1]):
-  curr_eigen = eigens[0:, i].reshape(len(eigens[0:, i]), 1)
-  eigenfaces = np.concatenate((eigenfaces, curr_eigen/np.linalg.norm(curr_eigen)), axis=1)
+  # Computes the eigenvalue of simplified covariance matrix
+  simp_eigens = eigval.eigen(simp_covariance)
 
-# Computes weight coefficients of each image in dataset
-weights = np.transpose(eigenfaces) @ difference
+  # Computes the eigenvalues from simplified eigenvalues
+  curr_simp_eigen = simp_eigens[0:, 0]
+  eigens = difference @ curr_simp_eigen.reshape(len(curr_simp_eigen), 1)
+  for i in range(1, simp_eigens.shape[1]):
+    curr_simp_eigen = simp_eigens[0:, i]
+    eigens = np.concatenate((eigens, difference @ curr_simp_eigen.reshape(len(curr_simp_eigen), 1)), axis=1)
 
-# Reconstruction
-curr_weight = weights[0:, 5]
-curr_eigenface = eigenfaces[0:, 0].reshape(len(eigenfaces[0:, 0]), 1)
-reconstruction = curr_weight[0] * curr_eigenface
-for i in range(1, eigenfaces.shape[1]):
-  curr_eigenface = eigenfaces[0:, i].reshape(len(eigenfaces[0:, i]), 1)
-  reconstruction += curr_weight[i] * curr_eigenface
-reconstruction += mean_face
+  # Normalize the eigenvalues to compute the eigenfaces
+  curr_eigen = eigens[0:, 0].reshape(len(eigens[0:, 0]), 1)
+  eigenfaces = curr_eigen/np.linalg.norm(curr_eigen)
+  for i in range(1, eigens.shape[1]):
+    curr_eigen = eigens[0:, i].reshape(len(eigens[0:, i]), 1)
+    eigenfaces = np.concatenate((eigenfaces, curr_eigen/np.linalg.norm(curr_eigen)), axis=1)
 
-recon_min = 999999
-recon_max = -999999
-for i in range(reconstruction.shape[0]):
-  if reconstruction[i][0] < recon_min:
-    recon_min = reconstruction[i][0]
-  if reconstruction[i][0] > recon_max:
-    recon_max = reconstruction[i][0]
-recon_range = recon_max - recon_min
-reconstruction = np.array([[(reconstruction[i][0] - recon_min)/recon_range * 255] for i in range(reconstruction.shape[0])])
-reconstruction = reconstruction.reshape(256, 256)
-reconstruction = np.around(reconstruction)
-reconstruction = reconstruction.astype("uint8")
+  # Computes weight coefficients of each image in dataset
+  weights = np.transpose(eigenfaces) @ difference
 
-# print(curr_weight)
-cv2.imshow("Testing", reconstruction)
-cv2.waitKey(0)
-
+  # Store the weights in a cache file
+  with open(dataset_path + "/eigen_cache.txt", 'w') as f:
+    for i in range(weights.shape[1]):
+      weight_str = ""
+      for j in range(len(weights[0:, i])):
+        weight_str += str(weights[0:, i][j]) + " "
+      weight_str += "\n"
+      f.write(weight_str)
+  
+  return weights
